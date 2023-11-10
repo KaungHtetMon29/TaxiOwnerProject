5c2777de9581b4d546d52332af3fd11f9999287e
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2023-11-10 14:13:58 +0630

    run

--- src/layout/normallistlayout.tsx
+++ src/layout/normallistlayout.tsx
@@ -1,6 +1,6 @@
 function NormallistLayout(props: { children: React.ReactNode }) {
   return (
-    <div className="px-4 gap-4 flex flex-col justify-center">
+    <div className="px-4 gap-4 flex flex-col items-center">
       {props.children}
     </div>
   );
